dafa93073738dc965af02c530cce22d9a1b07bbe
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-11-09 17:31:01 +1100

    Include website links for books
    In the gallery display.

--- src/components/BookGallery.js
+++ src/components/BookGallery.js
@@ -3,13 +3,15 @@ import React from 'react';
 const books = [
   {
     title: 'Chester, The Purple Dino: The Fruity City Expedition',
-    description: 'A kids picture book!',
+    description: 'A kids picture book for fruit names!',
     image: '/images/ChesterFruityBookCover_090324.jpg',
+    url: 'https://chesterthedino.com',
   },
   {
     title: 'Sydney Trains and Station Sketches',
     description: 'A collection of 40-odd freehand drawings!',
     image: '/images/SydneyTrainsBookcover_080924.png',
+    url: 'https://amzn.to/3AmmOp6', /* Amazon ref link*/
   },
   // Add more books here as needed
 ];
@@ -26,6 +28,14 @@ const BookGallery = () => {
               </div>
               <h3>{book.title}</h3>
               <p>{book.description}</p>
+              <a 
+              href={book.url} 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="order-link"
+            >
+              See this book!
+            </a>
             </div>
           ))}
         </div>
