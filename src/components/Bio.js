05d89651408ce81df8562e1cd41eb8fc83aeee7d
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-11-09 17:23:00 +1100

    Update bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -9,10 +9,21 @@ const Bio = () => {
             <p>
         Manoj Kumar lives in Sydney, Australia and fills his days with a kaleidoscope of activities. 
         From the 9-5 routine job to crafting captivating articles and stories, designing books, and doodling delightful randomness, 
-        he seamlessly weaves his passions into the fabric of his life. A podcaster, a vlogger, an avid traveler, and a spontaneous cook who often abandons recipes, 
+        he seamlessly weaves his passions into the fabric of his life. A podcaster, a vlogger, an avid traveler, a sticker creator, and a spontaneous cook who often abandons recipes, 
         Manoj finds joy in contemplating the mysteries of life and taking risks. 
         Nestled with his family, he embraces a life filled with gratitude, adding warmth and authenticity to everything he does.
             </p>
+            <p>
+            Check out all his activities here:{' '}
+        <a 
+          href="https://bento.me/emanoj" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="contact-link"
+        >
+          bento.me/emanoj
+        </a>.
+      </p>
         </div>
     </div>
   );
